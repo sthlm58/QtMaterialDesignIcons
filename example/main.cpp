94944b6207adb1abb28aaf74d982d6b19ad890d5
sthlm58 <sthlm58@gmail.com>
2018-07-24 18:08:05 +0200

    ADD: QML support using TTF font and Glyph names (no ligatures)
    Since Ligatures are not supported, mapping is done using Unicode characters extracted from the SVG.

--- example/main.cpp
+++ example/main.cpp
@@ -9,10 +9,21 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
+#if 1 // WIDGETS
+
 	auto label = new QLabel;
 	label->setPixmap(material::pixmap("eye", QSize(32, 32), Qt::darkGray));
 
 	label->show();
+
+#else // QML
+
+	QQmlApplicationEngine engine;
+	engine.load(QUrl(QStringLiteral("main.qml")));
+	if (engine.rootObjects().isEmpty())
+		return -1;
+
+#endif
 
 	return a.exec();
 }
